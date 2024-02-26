6532c06840ba53afc8188a09eb47b522b4b741c2
ANETI-Tunnisia <abdelwaheb_ammar@aneti.tn>
2024-02-26 10:28:44 +0100

    Ajouter Mes PremiersTestsAvecEntitiesCodeFirst(Section 8)

---
+++ MesPremiersTestsAvecEntitiesCodeFirst/DefaultContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesPremiersTestsAvecEntitiesCodeFirst
+{
+    public class DefaultContext:DbContext
+    {
+        public DefaultContext(DbContextOptions options):base(options) 
+        {
+                
+        }
+        protected DefaultContext()
+        {
+                
+        }
+        public DbSet<Droide> Droides { get; set; }
+    }
+}
---
+++ MesPremiersTestsAvecEntitiesCodeFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MesPremiersTestsAvecEntitiesCodeFirst;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+ConfigurationBuilder builder=new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json");
+var config=builder.Build();
+string connectionstring=config.GetConnectionString("DefaultContext");
+
+DbContextOptionsBuilder optionsBuilder=new DbContextOptionsBuilder();
+
+optionsBuilder.UseSqlServer(connectionstring);
+using(DefaultContext context=new DefaultContext(optionsBuilder.Options))
+{
+    var query=from droide in context.Droides
+              select droide;
+    foreach (var item in query.ToList())
+    {
+        Console.WriteLine(item.Matricule);
+    }
+}
+
+Console.WriteLine("Hello, World!");
