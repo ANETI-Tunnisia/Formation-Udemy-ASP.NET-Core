ec229d9b556648791f5c9194cbe23317a6fdea53
ANETI-Tunnisia <abdelwaheb_ammar@aneti.tn>
2024-03-17 23:26:07 +0100

    Section12/[NEW] - Comment on fait via c#

---
+++ TestAuthentificationFacebook/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TestAuthentificationFacebook.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TestAuthentificationFacebookContextConnection") ?? throw new InvalidOperationException("Connection string 'TestAuthentificationFacebookContextConnection' not found.");
+
+builder.Services.AddDbContext<TestAuthentificationFacebookContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<TestAuthentificationFacebook.Areas.Identity.Data.TestAuthentificationFacebookUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<TestAuthentificationFacebookContext>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
